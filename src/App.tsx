af629b2c792d7f87e36c8235d34fd85c564807a4
Han <rhaudgks04@gmail.com>
2026-01-14 23:51:35 +0900

    markup : 로그인 회원가입 페이지 마크업

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import { Outlet } from "react-router-dom";
 function App() {
     return (
         <>
-            <header>헤더</header>
-
             <Outlet />
-
-            <footer>푸터</footer>
         </>
     );
 }
