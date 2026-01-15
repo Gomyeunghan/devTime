2f68eb125d7b293317cdfc653bdb12e934612401
Han <rhaudgks04@gmail.com>
2026-01-16 01:03:35 +0900

    feat : input컴포넌트 feed백에 따른 스타일 수정기능 추가

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,3 +1,4 @@
+import { useState, type HTMLInputTypeAttribute } from "react";
 import S from "./Input.module.css";
 
 function Input({
@@ -5,20 +6,57 @@ function Input({
     inputLabel,
     isValid,
     feedBackText,
+    onChanage,
+    type,
+    available,
+    onBlur,
 }: {
     placeholder: string;
     inputLabel?: string;
     isValid: boolean;
+    available?: boolean;
     feedBackText?: string;
+    onChanage: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    type: HTMLInputTypeAttribute;
+    onBlur?: () => void;
 }) {
+    const [touched, setTouched] = useState(false);
+
+    const handleBlur = () => {
+        setTouched(true);
+        if (onBlur) {
+            onBlur();
+        }
+    };
+
+    const hasAvailableCheck = available !== undefined;
+
+    const showError = touched && !isValid;
+    const showSuccess = touched && isValid && available === true;
+
+    const showWarning =
+        hasAvailableCheck && touched && isValid && available !== true;
     return (
         <div className={S.container}>
             <div>{inputLabel}</div>
-            <div className={`${S.input} ${isValid ? "" : S.wrong}`}>
-                <input type="text" placeholder={placeholder} />
+            <div className={`${S.input} ${showError ? S.wrong : ""}`}>
+                <input
+                    type={type}
+                    placeholder={placeholder}
+                    onChange={onChanage}
+                    onBlur={handleBlur}
+                />
             </div>
             <span
-                className={`${S.feedBackText} ${isValid ? S.hidden : S.wrong}`}
+                className={`${S.feedBackText} ${
+                    showError
+                        ? S.wrong
+                        : showSuccess
+                        ? S.sucsses
+                        : showWarning
+                        ? S.wrong
+                        : S.hidden
+                }`}
             >
                 {feedBackText}
             </span>
