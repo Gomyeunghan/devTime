ba42fae7db0bd384aafde2af3a2fafa714bc91da
Han <rhaudgks04@gmail.com>
2026-01-19 22:09:30 +0900

    fix : api호출시 필요한 타입 분류

---
+++ src/api/types.ts
@@ -0,0 +1,24 @@
+export interface SignupRequest {
+    email: string;
+    nickname: string;
+    password: string;
+    confirmPassword: string;
+}
+
+export interface SignupResponse {
+    success: boolean;
+    message: string;
+}
+
+export interface ErrorResponse {
+    success: boolean;
+    error: {
+        message: string;
+        statusCode: number;
+    };
+}
+export interface CheckDuplicateResponse {
+    success: boolean;
+    available: boolean;
+    message: string;
+}
