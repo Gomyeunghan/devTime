7d3218cd21b9d5f2b359c5f2b0e707ee7f852aaa
Han <rhaudgks04@gmail.com>
2026-01-20 03:18:08 +0900

    fix : 피드백 상태 타입추가 및 메시지 상수파일 추가

---
+++ src/constants/messages/email.ts
@@ -0,0 +1,11 @@
+import type { FieldStatus } from "@/types/feedback.type";
+
+// constants/emailMessage.ts
+export const EMAIL_MESSAGE: Record<FieldStatus, string> = {
+    IDLE: "이메일 형식으로 입력해주세요.",
+    INVALID_FORMAT: "이메일 형식으로 입력해주세요.",
+    NEED_CHECK: "중복확인이 필요합니다.",
+    AVAILABLE: "",
+    DUPLICATE: "",
+    ERROR: "이메일 확인 중 오류가 발생했습니다.",
+} as const;
