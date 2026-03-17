f74eeb155876236e680858d7dd51e7c6994c00e5
Han <rhaudgks04@gmail.com>
2026-03-17 23:58:09 +0900

    add : integrate QueryClientProvider in main.tsx and remove unnecessary whitespace in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ function App() {
                         width: "100%",
                     }}
                 >
-                    {" "}
                     <Outlet />
                 </main>
             </TimerProvider>
